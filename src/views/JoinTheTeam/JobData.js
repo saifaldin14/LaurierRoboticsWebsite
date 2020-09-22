6802ace497011df87e9b013f9bb972816da96186
Saif Al-Din Ali <saifaldindeveloper@gmail.com>
2020-09-22 18:34:04 -0400

    Update jointheteam

---
+++ src/views/JoinTheTeam/JobData.js
@@ -0,0 +1,14 @@
+export const jobData = [
+  // {
+  //   position: "Software Engineer",
+  //   category: "Software",
+  //   description: "Basic understanding of computer vision, Tensorflow, python and deep learning neural networks",
+  //   contactEmail: "baig9620@mylaurier.ca"
+  // },
+  // {
+  //   position: "Hardware Engineer",
+  //   category: "Hardware",
+  //   description: "Experience with 3D modeling systems and autodesk inventor, an understanding of circuits and a willingness to learn",
+  //   contactEmail: "kawa0080@mylaurier.ca"
+  // }
+];
--- src/views/JoinTheTeam/JoinTheTeam.js
+++ src/views/JoinTheTeam/JoinTheTeam.js
@@ -1,63 +1,63 @@
 import React from "react";
 import "./JoinTheTeam.css";
+import { jobData } from './JobData';
 
-class JoinTheTeam extends React.Component {
-  render() {
+const JoinTheTeam = () => {
+
+  const renderJobs = (job) => {
     return (
-      <>
-        <div className="content">
-          <p>Even if you don't meet the qualifications feel free to apply.</p>
-          <table className="table table-dark">
-            <thead>
-              <tr>
-                <th scope="col">Position</th>
-                <th scope="col">Job Category</th>
-                <th scope="col">Qualifications</th>
-                {/*<th scope="col">Description</th>*/}
-                <th scope="col">Email</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <th scope="row">Software Engineer</th>
-                <td>Software</td>
-                <td>Basic understanding of computer vision, Tensorflow, python and deep learning neural networks</td>
-                {/* <td>N/A</td> */}
-                <td>baig9620@mylaurier.ca</td>
-              </tr>
-              <tr>
-                <th scope="row">Software Engineer</th>
-                <td>Software</td>
-                <td>Basic understanding of computer vision, Tensorflow, python and deep learning neural networks</td>
-                {/* <td>N/A</td> */}
-                <td>baig9620@mylaurier.ca</td>
-              </tr>
-              <tr>
-                <th scope="row">Hardware Engineer</th>
-                <td>Hardware</td>
-                <td>Experience with 3D modeling systems and autodesk inventor, an understanding of circuits and a willingness to learn</td>
-                {/* <td>N/A</td> */}
-                <td>kawa0080@mylaurier.ca</td>
-              </tr>
-              <tr>
-                <th scope="row">Hardware Engineer</th>
-                <td>Hardware</td>
-                <td>Experience with 3D modeling systems and autodesk inventor, an understanding of circuits and a willingness to learn</td>
-                {/* <td>N/A</td> */}
-                <td>kawa0080@mylaurier.ca</td>
-              </tr>
-            </tbody>
-          </table>
-          <p>Feel free to Email the address in the "EMAIL" column if you are interested in any of the positions available. If there are no positions listed, we still welcome you to reach out to us at
-          <a href="mailto:lauriobotics@gmail.com">
-              {' '}lauriobotics@gmail.com
-          </a>.
-          </p>
-          <p>Everyone, of different colours, regions, beliefs, and genders are welcome.</p>
-        </div>
-      </>
+      <tr>
+        <th scope="row">{job.position}</th>
+        <td>{job.category}</td>
+        <td>{job.description}</td>
+        {/* <td>N/A</td> */}
+        <td>{job.contactEmail}</td>
+      </tr>
     );
   }
+
+  const renderNoJobs = () => {
+    return (
+      <div>
+        <h2>There are no job openings</h2>
+        <h5>There aren't any jobs available right now</h5>
+        <p>We will update this page as soon as new jobs are available.</p>
+        <p>Please email lauriobotics@gmail.com if you have any questions</p>
+      </div>
+    );
+  }
+  return (
+    <>
+      <div className="content">
+        {jobData.length > 0 &&
+          <>
+            <p>Even if you don't meet the qualifications feel free to apply.</p>
+            <table className="table table-dark">
+              <thead>
+                <tr>
+                  <th scope="col">Position</th>
+                  <th scope="col">Job Category</th>
+                  <th scope="col">Qualifications</th>
+                  {/*<th scope="col">Description</th>*/}
+                  <th scope="col">Email</th>
+                </tr>
+              </thead>
+              <tbody>
+                {jobData.map(renderJobs)}
+              </tbody>
+            </table>
+            <p>Feel free to Email the address in the "EMAIL" column if you are interested in any of the positions available. If there are no positions listed, we still welcome you to reach out to us at
+          <a href="mailto:lauriobotics@gmail.com">
+                {' '}lauriobotics@gmail.com
+          </a>.
+          </p>
+            <p>Everyone, of different colours, regions, beliefs, and genders are welcome.</p>
+          </>
+        }
+        {jobData.length === 0 && renderNoJobs()}
+      </div>
+    </>
+  );
 }
 
 export default JoinTheTeam;
