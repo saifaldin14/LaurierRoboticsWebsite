e5d192ddc5d8e13319f2024b6f932de31160c7bc
hackerrank <me@hackerrank.com>
2020-10-02 20:04:25 -0400

    Update EventsData.js

--- src/views/Events/EventsData.js
+++ src/views/Events/EventsData.js
@@ -1,24 +1,24 @@
 import image from '../../assets/img/hardwarebg.jpg';
 
 export const cardInfo = [
-  // {
-  //   image: image,
-  //   title: "Welcome Event",
-  //   date: "September 10 7:00 - 8:00 PM EST",
-  //   description: "First event of the year",
-  // },
-  // {
-  //   image: image,
-  //   title: "Robot Strategy Meeting",
-  //   date: "September 14 6:30 - 7:30 PM EST",
-  //   description: "Meeting to discuss the strategy and timeline for building the robot",
-  // },
-  // {
-  //   image: "https://res.cloudinary.com/engineering-com/image/upload/w_640,h_640,c_limit,q_auto,f_auto/bigstock-Architect-Engineer-Meeting-Peo-101361728_oxgf0s.jpg",
-  //   title: "Hardware Team Meeting",
-  //   date: "October 23 4:00 - 5:00 PM EST",
-  //   description: "Meeting to discuss 3D model of the robot and budget meeting with the Finance Team",
-  // },
+  {
+    image: image,
+    title: "Welcome Event",
+    date: "September 10 7:00 - 8:00 PM EST",
+    description: "First event of the year",
+  },
+  {
+    image: image,
+    title: "Robot Strategy Meeting",
+    date: "September 14 6:30 - 7:30 PM EST",
+    description: "Meeting to discuss the strategy and timeline for building the robot",
+  },
+  {
+    image: "https://res.cloudinary.com/engineering-com/image/upload/w_640,h_640,c_limit,q_auto,f_auto/bigstock-Architect-Engineer-Meeting-Peo-101361728_oxgf0s.jpg",
+    title: "Hardware Team Meeting",
+    date: "October 23 4:00 - 5:00 PM EST",
+    description: "Meeting to discuss 3D model of the robot and budget meeting with the Finance Team",
+  },
 ];
 export const videoURL = [
   {
