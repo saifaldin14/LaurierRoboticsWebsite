bf61d23f7f24d8f88cd0493092c72026e29f3194
saifaldin14 <saifaldindeveloper@gmail.com>
2021-01-08 17:12:52 -0500

    Added new sponsor

--- src/views/Sponsership/Sponsership.js
+++ src/views/Sponsership/Sponsership.js
@@ -1,6 +1,7 @@
 import React from "react";
-import DQ from 'assets/img/DQ.jpg';
-import { } from "./Sponsership.css";
+import DQ from "assets/img/DQ.jpg";
+import Signs from "assets/img/Signs.png";
+import {} from "./Sponsership.css";
 // reactstrap components
 /*import {
   Card,
@@ -17,22 +18,32 @@ class Sponsership extends React.Component {
     return (
       <>
         <div className="content">
-        <h2 id="h2">Thank You!</h2>
+          <h2 id="h2">Thank You!</h2>
           <hr />
-          <p>We'd love to give a big thank you to our wonderful sponsors that support us!</p>
-          <ul>
-          <li id="imgStyle">
-            <a href="" target="blank">
-              <img id='igmedia' src={DQ} alt="DQ logo" />
-            </a>
-          </li>
-        </ul>
+          <p>
+            We'd love to give a big thank you to our wonderful sponsors that
+            support us!
+          </p>
+          <div id="divSponsor">
+            <img id="imgSponsor" src={DQ} alt="DQ logo" />
+            <img id="imgSponsor" src={Signs} alt="Signs logo" />
+          </div>
+          {/* <ul id="ul_sponsors">
+            <li>
+              <img id="imgSponsor" src={DQ} alt="DQ logo" />
+            </li>
+            <li>
+              <img id="imgSponsor" src={Signs} alt="Signs logo" />
+            </li>
+          </ul> */}
           <h2 id="h2">Join Us!</h2>
           <hr />
-          <p>Here at Laurier Robotics we are a group of
-          like-minded students passionate about technology
-          and robotics. If you would like to be part of this
-            incredible team please don't hesitate to <a href="/admin/contactus">contact us</a>!</p>
+          <p>
+            Here at Laurier Robotics we are a group of like-minded students
+            passionate about technology and robotics. If you would like to be
+            part of this incredible team please don't hesitate to{" "}
+            <a href="/admin/contactus">contact us</a>!
+          </p>
         </div>
       </>
     );
